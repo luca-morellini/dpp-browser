58ef3b0805220673fd441e5ff48e425be595dede
Luca Morellini <lucamorellini96@gmail.com>
2025-01-08 18:08:47 +0100

    First json data print in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,48 @@
 import { Link } from "react-router-dom";
+import data from "../data.json"
+import Dropdown from "../components/Dropdown";
 
 function Home() {
-    return (
-        <h1>Home page</h1>
-    )
+  return (
+    <>
+      <h1>Digital Product Passport</h1>
+      <div className="prop-id">
+        <strong>Lotto: </strong>{data.Batch.BatchID}
+      </div>
+      {data.Batch.BatchProperties.map(property => (
+        <div key={property.PropertyName}>
+          <Dropdown property={property} />
+        </div>
+      ))}
+
+      <div className="prop-id">
+        <strong>Oggetto: </strong>{data.Item.ItemID}
+      </div>
+      {data.Item.ItemProperties.map(property => (
+        <div key={property.PropertyName}>
+          <Dropdown property={property} />
+        </div>
+      ))}
+
+      <div className="prop-id">
+        <strong>Famiglia Prodotto: </strong>{data.ProductFamily.ProductFamilyID}
+      </div>
+      {data.ProductFamily.ProductFamilyProperties.map(property => (
+        <div key={property.PropertyName}>
+          <Dropdown property={property} />
+        </div>
+      ))}
+
+      <div className="prop-id">
+        <strong>Company: </strong>{data.Company.CompanyID}
+      </div>
+      {data.Company.CompanyProperties.map(property => (
+        <div key={property.PropertyName}>
+          <Dropdown property={property} />
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default Home;
