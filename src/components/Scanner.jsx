319619603820a91766f443e41005b79f5c9b2434
Luca Morellini <lucamorellini96@gmail.com>
2025-01-13 16:58:18 +0100

    First working qr-reader added

---
+++ src/components/Scanner.jsx
@@ -0,0 +1,39 @@
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Scanner() {
+  const [scanResult, setScanResult] = useState(null);
+
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner('reader',{
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    })
+  
+    scanner.render(success, error);
+  
+    function success(result) {
+      scanner.clear();
+      setScanResult(result); 
+    }
+  
+    function error(err){
+      console.warn(err);
+    }
+  }, []);
+
+  return (
+    <div>
+      { scanResult
+      ? <div>Success: {scanResult}</div>
+      : <div id="reader"></div>
+      }
+    </div>
+  )
+}
+
+export default Scanner;
--- src/pages/QrReader.jsx
+++ src/pages/QrReader.jsx
@@ -1,8 +1,10 @@
+import Scanner from "../components/Scanner";
 
 function QrReader() {
   return (
     <>
       <h1>Add QR-reader here</h1>
+      <Scanner />
     </>
   )
 }
