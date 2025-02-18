a771759e61d2740eb2ec5294539285d65c3bbfde
Luca Morellini <lucamorellini96@gmail.com>
2025-02-18 10:29:47 +0100

    Added language selection

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,36 @@ import InputForm from "./components/InputForm";
 import LinkedButton from "./components/LinkedButton";
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-//import json_data from "./data_template.json"
+import Select from 'react-flags-select';
+//import json_template from "./data_template.json"
+
+const languages = {
+  IT: "IT",
+  GB: "UK",
+  ES: "ES",
+  FR: "FR",
+};
 
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [back_button_visible, setBackButtonVisible] = useState(false);
-  const [queue, setQueue] = useState([]);
+  const [list, setList] = useState([]);
+  const [selectedCountry, setCountry] = useState('IT');
 
   const pushElement = (new_element) => {
     if (new_element) {
-      setQueue([...queue, new_element]);
+      setQList([...list, new_element]);
       setBackButtonVisible(true);
     }
   };
 
   const popElement = () => {
-    if (queue.length > 0) {
-      const element = queue.shift();
-      setQueue([...queue]);
-      setBackButtonVisible(queue.length > 0);
+    if (list.length > 0) {
+      const element = list.shift();
+      setList([...list]);
+      setBackButtonVisible(list.length > 0);
       return element;
     }
     return null;
@@ -35,7 +44,7 @@ function App() {
   };
 
   const getApiUrl = ({url, batch_code, item_code, productfamily_code, company_code}) => {
-    let api_url = `${url}/browser-protocol/get_batch_details/${batch_code}/${item_code}/${productfamily_code}/${company_code}/?format=json`;
+    let api_url = `${url}/browser-protocol/get_batch_details/${batch_code}/${item_code}/${productfamily_code}/${company_code}/${languages[selectedCountry]}/?format=json`;
     if (!api_url.startsWith("http://")) {
       api_url = `http://${api_url}`;
     }
@@ -72,6 +81,13 @@ function App() {
 
   return (
     <div className="container">
+      <Select
+        className="mt-2 col-md-1"
+        countries={["IT", "GB", "ES", "FR"]}
+        customLabels={{ "IT": "Italiano", "GB": "English", "ES": "Español", "FR": "Français" }}
+        onSelect={setCountry}
+        selected={selectedCountry}
+      />
       <h1 className="title">Digital Product Passport</h1>
 
       <InputForm getApiUrl={getApiUrl} fetchData={fetchData}/>
